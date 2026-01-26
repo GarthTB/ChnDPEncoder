e0ea751f18aa6e61af26b695369ae7404dd5d185
Garth TB | 天卜 <g-art-h@outlook.com>
2026-01-27 01:24:55 +0800

    升级Tomlyn依赖并优化配置加载逻辑

--- ChnDPEncoder/Models/ConfigModel.cs
+++ ChnDPEncoder/Models/ConfigModel.cs
@@ -9,31 +9,33 @@ using Tomlyn.Model;
 /// <param name="Dict"> 前缀树词库 </param>
 /// <param name="SpaceCodes"> 需要空格分隔的编码 </param>
 /// <param name="Texts"> 待编码文本路径 </param>
-/// <param name="Layout"> 键盘布局表 </param>
+/// <param name="Layout"> （可选）键盘布局表 </param>
 internal sealed record ConfigModel(
     CostMap Costs,
     TrieDict Dict,
     FrozenSet<(string, char)> SpaceCodes,
     IEnumerable<string> Texts,
-    LayoutMap Layout)
+    LayoutMap? Layout)
 {
     /// <summary> 从TOML文件加载配置 </summary>
     public static ConfigModel FromToml(string path) {
-        var toml = Toml.ToModel(File.ReadAllText(path));
-        CostMap costs = toml["cost_map"] is string costsPath
+        var toml = File.ReadAllText(path);
+        var model = Toml.ToModel(toml);
+        CostMap costs = model["cost_map"] is string costsPath
             ? new(costsPath)
             : throw new ArgumentException("键对开销表路径缺失");
-        TrieDict dict = toml["rime_dict"] is string dictPath
+        TrieDict dict = model["rime_dict"] is string dictPath
             ? new(dictPath, costs, out var spaceCodes)
             : throw new ArgumentException("RIME词库路径缺失");
-        var sTexts = toml["texts"] is TomlArray { Count: > 0 } oTexts
-            ? oTexts.Select(static obj =>
-                obj as string ?? throw new ArgumentException("待编码文本路径格式错误"))
+        var sTexts = model["texts"] is TomlArray { Count: > 0 } oTexts
+            ? ParseTomlArray(oTexts, "待编码文本路径")
             : throw new ArgumentException("待编码文本路径缺失");
-        var sKeys = toml["layout"] is TomlArray { Count: 14 } oKeys
-            ? oKeys.Select(static obj => obj as string ?? throw new ArgumentException("键盘布局格式错误"))
-            : throw new ArgumentException("键盘布局缺失或格式错误");
-        LayoutMap layout = new(sKeys.ToArray());
+        LayoutMap? layout = model["layout"] is TomlArray { Count: 14 } oKeys
+            ? new(ParseTomlArray(oKeys, "键盘布局").ToArray())
+            : null;
         return new(costs, dict, spaceCodes, sTexts, layout);
+
+        static IEnumerable<string> ParseTomlArray(TomlArray arr, string name) =>
+            arr.Select(obj => obj as string ?? throw new ArgumentException($"{name}数组格式错误"));
     }
 }
